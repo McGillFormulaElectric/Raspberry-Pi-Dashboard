2469f5dd7b52f631e64804030e0847e0068f10e4
diviiniity <matpestel@gmail.com>
2026-04-25 21:34:26 -0400

    changed perms for bash script

--- config_scripts/run_app.sh
+++ config_scripts/run_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+chmod +x /home/mcgillformulaelectric/Raspberry-Pi-Dashboard/config_scripts/run_app.sh
 cd /home/mcgillformulaelectric/Raspberry-Pi-Dashboard || exit
 
 source venv/bin/activate 
